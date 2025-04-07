11499fbf61998f03d66bb35d6db571f996ea9c2d
わたあめえ <Wataamee777@gmail.com>
2025-04-07 15:53:20 +0900

    Create script.js

---
+++ api/script.js
@@ -0,0 +1,10 @@
+ // APIからサーバーステータスを取得
+    fetch('https://api.mcsrvstat.us/2/play.kotoca.net')
+      .then(res => res.json())
+      .then(data => {
+        // JSONデータをpreタグ内に表示
+        document.getElementById('json-display').textContent = JSON.stringify(data, null, 2);
+      })
+      .catch(() => {
+        document.getElementById('json-display').textContent = 'サーバーに接続できませんでした';
+      });
